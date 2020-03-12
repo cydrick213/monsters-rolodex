45fd1b1c1c51d0616fb2e49ef080ea6a8a235b40
cydrick <cydrick@ajio.co.jp>
2020-03-12 16:59:34 +0800

    Updates

---
+++ precache-manifest.0ea86dc19e343e7421317157dc620546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760a34148e80e4a0fa6fe90c21dd392c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "58d1e507c68c96469c38",
+    "url": "/monsters-rolodex/static/css/main.b9e7787a.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d1e507c68c96469c38",
+    "url": "/monsters-rolodex/static/js/main.9b396b5d.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
